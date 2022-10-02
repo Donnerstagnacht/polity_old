581e851b54d019a54467f5449b83b24ba692c839
Donnerstagnacht <35246325+Donnerstagnacht@users.noreply.github.com>
2022-10-02 16:53:42 +0200

    Edge function receives data from trigger

--- supabase/functions/send-web-push/index.ts
+++ supabase/functions/send-web-push/index.ts
@@ -1,7 +1,4 @@
 // deno-lint-ignore-file no-explicit-any
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
 
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
 import { supabaseClient } from '../_shared/supabaseClient.ts'
@@ -60,15 +57,10 @@ serve(async (req: any) => {
       { headers: { "Content-Type": "application/json" } },
     )
   }
-
-/*   return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  ) */
 })
 
 // To invoke:
-/*  curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs' \
-   --header 'Content-Type: application/json' \
-   --data '{"name":"Functions"}' */
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
--- supabase/migrations/007_notifications_functions.sql
+++ supabase/migrations/007_notifications_functions.sql
@@ -224,30 +224,36 @@ RETURN QUERY
 END;
 $$;
 
--- Trigger for Offline notifications
+
+
+-- Notification Trigger to activate WebPush Edge functions
 drop extension if exists http;
-create extension http with schema extensions;
-
--- Copy users to profile
-CREATE OR REPLACE FUNCTION copy_notification_to_group()
-RETURNS TRIGGER AS $$
-  BEGIN
-    PERFORM insert_notification_from_groups(
-      new.notifier,
-      '7e93a265-a4df-4dfc-b9b3-875b08960ece', --new.notifying,
-      new.handler,
-      new.title,
-      new.message,
-      new.type,
-      new.for_admins
-    );
-
-    RETURN NEW;
-  END;
-$$
-LANGUAGE plpgsql SECURITY DEFINER;
+create extension http;
 
 DROP TRIGGER IF EXISTS copy_notification on public.notifications_of_user;
+DROP FUNCTION IF EXISTS copy_notification_to_group();
+
+CREATE OR REPLACE FUNCTION copy_notification_to_group()
+RETURNS TRIGGER AS $$
+  declare
+    data_as_text text;
+  BEGIN
+    data_as_text := row_to_json(NEW.*)::text;
+    perform http((
+      'POST',
+      'https://ehsbtpkdyyzoipqdmmvv.functions.supabase.co/send-web-push',
+      ARRAY[
+        http_header('Authorization', 'Bearer ' || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoc2J0cGtkeXl6b2lwcWRtbXZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQxNjk1MzIsImV4cCI6MTk2OTc0NTUzMn0.RCp3gX4QRQx4ylZLTj98aMvjYVfxsPeSIXksOrBm6RI')
+        --http_header('function-secret',  func_secret)
+      ],
+      'application/json',
+      data_as_text
+      ));
+    RETURN NEW;
+  END;
+$$ LANGUAGE PLPGSQL SECURITY DEFINER
+SET search_path = extensions, public, pg_temp;
+
 CREATE TRIGGER copy_notification
 AFTER INSERT ON public.notifications_of_user
 FOR EACH ROW EXECUTE PROCEDURE copy_notification_to_group();
