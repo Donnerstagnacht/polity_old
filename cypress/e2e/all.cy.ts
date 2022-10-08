c08a7290c7966412dc2969d31a74d4e8a19776d8
Donnerstagnacht <35246325+Donnerstagnacht@users.noreply.github.com>
2022-10-08 12:55:56 +0200

    add follow group to all.cy

--- cypress/e2e/all.cy.ts
+++ cypress/e2e/all.cy.ts
@@ -2,10 +2,9 @@ import './03-authentification/authentification.cy.ts'
 import './04-profile/profile.cy.ts'
 import './05-profile follow/followProfile.cy.ts'
 import './06-group/createGroup.cy.ts'
-/* 
-
 import './07-group follow/followGroup.cy'
-import './08-group membership/groupMembership.cy'
+
+/* import './08-group membership/groupMembership.cy'
 import './09-chat/chat.cy'
 import './09-chat/chat.Group.cy'
 import './10-search/search.cy.ts'
