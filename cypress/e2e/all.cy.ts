2e3fb0831ae54698abcff980552dacef920d7949
Donnerstagnacht <35246325+Donnerstagnacht@users.noreply.github.com>
2022-10-10 14:20:17 +0200

    stabile search tests cy.intercept()

--- cypress/e2e/all.cy.ts
+++ cypress/e2e/all.cy.ts
@@ -5,8 +5,9 @@ import './06-group/createGroup.cy.ts'
 import './07-group follow/followGroup.cy'
 import './08-group membership/groupMembership.cy'
 import './09-chat/chat.cy'
-/* 
-
 import './09-chat/chat.Group.cy'
 import './10-search/search.cy.ts'
+
+/* 
+
 import './11-notify/11-notify.cy' */
