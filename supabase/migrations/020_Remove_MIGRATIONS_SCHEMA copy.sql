db5a79677402c3effe8f03c91ee556b6550a99da
Donnerstagnacht <35246325+Donnerstagnacht@users.noreply.github.com>
2022-10-25 02:01:32 +0200

    Reset table & functions

--- supabase/migrations/020_Remove_MIGRATIONS_SCHEMA copy.sql
+++ supabase/migrations/020_Remove_MIGRATIONS_SCHEMA copy.sql
@@ -1,8 +1,8 @@
 -- creating a trigger which deletes entries into the schema_migrations table once they are inserted
 -- This is needed for a hard reset since dp push does not apply migrations which are already added
 -- to the schema_migrations table
-DROP SCHEMA public CASCADE;
-
+DROP SCHEMA if exists public CASCADE;
+CREATE SCHEMA public;
 DROP TRIGGER IF EXISTS trigger_migration_row_delete ON supabase_migrations.schema_migrations;
 DROP FUNCTION IF EXISTS supabase_migrations.signup_copy_to_users_table();
 
