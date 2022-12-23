5ae27e12d60faa4a15dd037d6a934868adfa1af7
Donnerstagnacht <35246325+Donnerstagnacht@users.noreply.github.com>
2022-12-23 15:06:30 +0100

    Granting all for public schema to postgres

--- supabase/migrations/030_transactions_schema.sql
+++ supabase/migrations/030_transactions_schema.sql
@@ -2,6 +2,7 @@
 -- and make sure thy can only be called by fullfilling all function calls of a transaction
 
 CREATE SCHEMA IF NOT EXISTS transactions;
-GRANT USAGE ON SCHEMA transactions TO postgres;
-GRANT all ON SCHEMA public TO postgres;
-GRANT ALL ON ALL FUNCTIONS IN SCHEMA transactions TO postgres;
+--GRANT USAGE ON SCHEMA transactions TO postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA transactions TO postgres;
+--GRANT ALL ON ALL FUNCTIONS IN SCHEMA transactions TO postgres;
