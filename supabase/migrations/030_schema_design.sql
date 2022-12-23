d555caa7b1924382e37b4e4fa4ad1744ab161c5d
Donnerstagnacht <35246325+Donnerstagnacht@users.noreply.github.com>
2022-12-23 13:44:51 +0100

    Add transactions schema

---
+++ supabase/migrations/030_schema_design.sql
@@ -0,0 +1,7 @@
+-- Creates a "transaction" schema which is not accessible to the API to protect functions
+-- and make sure thy can only be called by fullfilling all function calls of a transaction
+
+CREATE SCHEMA IF NOT EXISTS transactions;
+GRANT USAGE ON SCHEMA transactions TO postgres;
+GRANT all ON SCHEMA public TO postgres;
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA transactions TO postgres;
